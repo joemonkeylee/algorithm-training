4764383269fdd8c69147fa139d04fbd47e5d60ab
joemonkeylee <joemonkeylee@sina.com>
2023-02-19 22:32:05 +0800

    no message

---
+++ csharp/DailyCheck.FormTools/Questions/768. 最多能完成排序的块 II.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCheck.FormTools.Questions
+{
+    internal class _768
+    {
+
+            public int maxChunksToSorted(int[] arr)
+            {
+                List<int> stack = new List<Integer>();
+                for (int num : arr)
+                {
+                    if (!stack.isEmpty() && num < stack.getLast())
+                    {
+                        int head = stack.removeLast();
+                        while (!stack.isEmpty() && num < stack.getLast()) stack.removeLast();
+                        stack.addLast(head);
+                    }
+                    else stack.addLast(num);
+                }
+                return stack.size();
+            }
+        }
+}
