18ac23c516ac7a2a99cecabd55fc2d3734c28774
joemonkeylee <joemonkeylee@sina.com>
2023-02-27 22:07:32 +0800

    day14

--- csharp/DailyCheck.FormTools/Questions/100. 相同的树.cs
+++ csharp/DailyCheck.FormTools/Questions/100. 相同的树.cs
@@ -9,35 +9,9 @@ namespace Questions
         {
             public bool IsSameTree(TreeNode p, TreeNode q)
             {
-                var list1 = new List<TreeNode>();
-                list1 = GetListFromNodes(p, list1);
-
-                var list2 = new List<TreeNode>();
-                list2 = GetListFromNodes(q, list2);
-
-                if (list1.Count != list2.Count())
-                {
-                    return false;
-                }
-                else
-                {
-                    for (int i = 0; i < list1.Count; i++)
-                    {
-                        if (list1[i].val != list2[i].val)
-                        {
-                            return false;
-                        }
-                    }
-                }
-                return true;
-            }
-
-            private List<TreeNode> GetListFromNodes(TreeNode node, List<TreeNode> result)
-            {
-                result.Add(node);
-                GetListFromNodes(node.left, result);
-                GetListFromNodes(node.right, result);
-                return result;
+                if (p == null || q == null) return p == q;
+                if (p.val != q.val) return false;
+                return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
             }
         }
     }
