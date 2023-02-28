0df536eeeeb210fa73421b2b7063989fb94e9282
joemonkeylee <joemonkeylee@sina.com>
2023-02-28 23:02:21 +0800

    no message

---
+++ csharp/DailyCheck.FormTools/Questions/129. 求根节点到叶节点数字之和.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace DailyCheck.FormTools.Questions
+{
+    internal class _129
+    {
+        public int SumNumbers(Models.TreeNode root)
+        {
+
+            return dfs(root, 0);
+        }
+
+        public int dfs(Models.TreeNode root, int prevSum)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int sum = prevSum * 10 + root.val;
+            if (root.left == null && root.right == null)
+            {
+                return sum;
+            }
+            else
+            {
+                return dfs(root.left, sum) + dfs(root.right, sum);
+            }
+        }
+    }
+}
