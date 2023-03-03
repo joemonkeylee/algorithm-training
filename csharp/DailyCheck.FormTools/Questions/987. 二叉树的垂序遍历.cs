0bc614e3f8c5eef7325216144736bcb1ae18cc07
joemonkeylee <joemonkeylee@sina.com>
2023-03-03 23:00:39 +0800

    no message

--- csharp/DailyCheck.FormTools/Questions/987. 二叉树的垂序遍历.cs
+++ csharp/DailyCheck.FormTools/Questions/987. 二叉树的垂序遍历.cs
@@ -1,12 +1,18 @@
-﻿### 思路
+﻿using TreeNode = DailyCheck.FormTools.Models.TreeNode;
 
-没做出来 mark
 
-### 代码
+namespace DailyCheck.FormTools.Questions
+{
+    internal class _987
+    {
+        //给你二叉树的根结点 root ，请你设计算法计算二叉树的 垂序遍历 序列。
+        //对位于(row, col) 的每个结点而言，其左右子结点分别位于(row + 1, col - 1) 和(row + 1, col + 1) 。树的根结点位于(0, 0) 。
+        //二叉树的 垂序遍历 从最左边的列开始直到最右边的列结束，按列索引每一列上的所有结点，形成一个按出现位置从上到下排序的有序列表。如果同行同列上有多个结点，则按结点的值从小到大进行排序。
+        //返回二叉树的 垂序遍历 序列。
 
-```c#
-
-    public IList<IList<int>> VerticalTraversal(TreeNode root)
+        public class Solution
+        {
+            public IList<IList<int>> VerticalTraversal(TreeNode root)
             {
                 List<Tuple<int, int, int>> nodes = new List<Tuple<int, int, int>>();
                 DFS(root, 0, 0, nodes);
@@ -52,9 +58,6 @@
                 DFS(node.left, row + 1, col - 1, nodes);
                 DFS(node.right, row + 1, col + 1, nodes);
             }
-
-```
-
-**复杂度分析**
-- 时间复杂度：O(nlogn)
-- 空间复杂度：O(n)
+        }
+    }
+}
