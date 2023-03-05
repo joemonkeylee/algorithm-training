95a26a1933896173bafe9931027285394ba5941a
joemonkeylee <joemonkeylee@sina.com>
2023-03-05 22:41:26 +0800

    day20

---
+++ csharp/DailyCheck.FormTools/Questions/347. 前 K 个高频元素.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCheck.FormTools.Questions
+{
+    internal class _347
+    {
+        //给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
+
+        //示例 1:
+        //输入: nums = [1,1,1,2,2,3], k = 2
+        //输出: [1,2]
+
+        //示例 2:
+        //输入: nums = [1], k = 1
+        //输出: [1]
+
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.Keys.Contains(nums[i]))
+                {
+                    dict[nums[i]]++;
+                }
+                else
+                {
+                    dict[nums[i]] = 1;
+                }
+            }
+            return dict.OrderByDescending(x => x.Value).Take(k).Select(x => x.Key).ToArray();
+        }
+    }
+}
